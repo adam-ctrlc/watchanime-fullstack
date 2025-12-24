035c69b7f8f960fcaee0256e24bfbc8c29157c5c
adam <174100539+adam-ctrlc@users.noreply.github.com>
2025-12-24 13:09:31 +0800

    latest version of next.js

--- src/app/trending/page.jsx
+++ src/app/trending/page.jsx
@@ -1,20 +1,20 @@
-'use client';
-import { Suspense } from 'react';
-import AnimePaginatedList from '@/app/components/AnimePaginatedList';
-import LoadingSpinner from '@/app/components/LoadingSpinner';
+"use client";
+import { Suspense } from "react";
+import AnimePaginatedList from "@/components/features/anime/AnimePaginatedList";
+import LoadingSpinner from "@/components/status/LoadingSpinner";
 
 export default function TrendingAnimePage() {
   return (
     <Suspense
       fallback={
-        <div className='flex justify-center items-center min-h-[50vh]'>
-          <LoadingSpinner size='large' />
+        <div className="flex justify-center items-center min-h-[50vh]">
+          <LoadingSpinner size="large" />
         </div>
       }
     >
       <AnimePaginatedList
-        title='Trending Anime'
-        endpoint='/trending/anime'
+        title="Trending Anime"
+        endpoint="/trending/anime"
         itemsPerPage={24}
       />
     </Suspense>
